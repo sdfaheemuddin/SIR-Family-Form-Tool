d21cdd5c57f4ec7a0d7b63800b6f589205c1beaa
Syed Faheemuddin <sd.faheemuddin@gmail.com>
2026-07-06 17:55:05 +0530

    Bump add new popup module imports

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 import { loadState, saveState } from "./storage.js";
 import { initUI } from "./ui.js";
 import { normalizeApplicant, normalizePerson } from "./core.js";
-import { initPersonPopupOverrides } from "./popups/person-popup.js?v=26-07-06-family-filter";
-import { initApplicantPopupOverrides } from "./popups/applicant-popup.js?v=26-07-06-family-filter";
+import { initPersonPopupOverrides } from "./popups/person-popup.js?v=26-07-06-add-new-person";
+import { initApplicantPopupOverrides } from "./popups/applicant-popup.js?v=26-07-06-add-new-person";
 
 const loaded = loadState();
 const rawPeople = Array.isArray(loaded.people) ? loaded.people : [];
