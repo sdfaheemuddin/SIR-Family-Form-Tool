51d4862999e69fe4b1174958aa9ad3d8e35e8b3b
Syed Faheemuddin <sd.faheemuddin@gmail.com>
2026-07-08 02:36:50 +0530

    Bump popup imports for export button fallback

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 import { loadState, saveState } from "./storage.js";
 import { initUI } from "./ui.js";
 import { normalizeApplicant, normalizePerson } from "./core.js";
-import { initPersonPopupOverrides } from "./popups/person-popup.js?v=26-07-07-10";
-import { initApplicantPopupOverrides } from "./popups/applicant-popup.js?v=26-07-07-10";
+import { initPersonPopupOverrides } from "./popups/person-popup.js?v=26-07-07-11";
+import { initApplicantPopupOverrides } from "./popups/applicant-popup.js?v=26-07-07-11";
 
 const loaded = loadState();
 const rawPeople = Array.isArray(loaded.people) ? loaded.people : [];
