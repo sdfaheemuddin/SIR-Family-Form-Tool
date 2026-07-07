4c67f683020f1c57f21bf1a6aab98ebb4cf7637b
Syed Faheemuddin <sd.faheemuddin@gmail.com>
2026-07-08 02:29:29 +0530

    Bump popup imports for family tree export

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 import { loadState, saveState } from "./storage.js";
 import { initUI } from "./ui.js";
 import { normalizeApplicant, normalizePerson } from "./core.js";
-import { initPersonPopupOverrides } from "./popups/person-popup.js?v=26-07-07-9";
-import { initApplicantPopupOverrides } from "./popups/applicant-popup.js?v=26-07-07-9";
+import { initPersonPopupOverrides } from "./popups/person-popup.js?v=26-07-07-10";
+import { initApplicantPopupOverrides } from "./popups/applicant-popup.js?v=26-07-07-10";
 
 const loaded = loadState();
 const rawPeople = Array.isArray(loaded.people) ? loaded.people : [];
