c8a8095c9a4433c020adddca9012ae36e3b6ba68
Syed Faheemuddin <sd.faheemuddin@gmail.com>
2026-07-08 02:36:32 +0530

    Bump UI import for export button fallback

--- ui.js
+++ ui.js
@@ -2,8 +2,8 @@
 import { buildReadonly, formatAadhaar, onlyDigits } from "./core.js";
 import { backupState, clearState } from "./storage.js";
 import { downloadJson } from "./importExport.js";
-import { initFileActions } from "./popups/file-actions/file-actions.js?v=26-07-07-10";
-import { initFamilyTree, renderFamilyTree } from "./family-tree.js?v=26-07-07-10";
+import { initFileActions } from "./popups/file-actions/file-actions.js?v=26-07-07-11";
+import { initFamilyTree, renderFamilyTree } from "./family-tree.js?v=26-07-07-11";
 
 let stateRef;
 let commitRef;
